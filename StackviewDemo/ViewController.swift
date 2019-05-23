8f60198f24e067de79354a96ddbe807fde808aa9
Miguel A. Zapata <miguel.zapatae@gmail.com>
2019-05-22 23:04:35 -0400

    Initial Commit

---
+++ StackviewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackviewDemo
+//
+//  Created by Miguel Angel Zapata Elizalde on 5/22/19.
+//  Copyright © 2019 Miguel A. Zapata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
